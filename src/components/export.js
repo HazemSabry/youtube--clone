90b2e62886a1aa9efcc7fc70fcf4cc0b6762fd5d
HazemSabry <hazemsabry2002@gmail.com>
2023-11-26 19:13:25 +0200

    The pages structure

---
+++ src/components/export.js
@@ -0,0 +1,5 @@
+export { defaults as Navbar } from "./Navbar";
+export { defaults as Feed } from "./Feed";
+export { defaults as ChannelDetail } from "./ChannelDetail";
+export { defaults as SearchFeed } from "./SearchFeed";
+export { defaults as VideoDetail } from "./VideoDetail";
